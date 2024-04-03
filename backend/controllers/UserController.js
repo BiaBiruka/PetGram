4a7225456e16d0bd2094cea6fc85840e3ce057ea
Beatriz Salvador Parolin <beatrizparolin@ibm.com>
2024-04-03 14:59:57 -0300

    secret, user controller

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,20 @@
+const User = require('../models/User');
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const jwtSecret = process.env.JWT_SECRET;
+
+// Gerar token de usuário
+const generateToken = (id) => {
+    // Faz a autenticação passando o id e o secret do .env
+    return jwt.sign({id}, jwtSecret, {
+        expiresIn: '7d' // Autenticacão dura 7 dias
+    })
+}
+
+
+// Cadastro e login
+const register = async(request, response) => {
+    
+}
