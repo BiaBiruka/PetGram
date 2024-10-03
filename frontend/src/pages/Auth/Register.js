8a870bc3fd6b2e082f558fc02fc0cdd980998800
Beatriz Salvador Parolin <beatrizparolin@ibm.com>
2024-10-03 16:03:00 -0300

    feat: register screen, global style for forms

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,7 +1,66 @@
+import { useState, useEffect, React } from 'react';
 import './index.css';
 
+// Components
+import { Link } from 'react-router-dom';
+
 const Register = () => {
-  return <div>Register</div>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+
+    console.log(user);
+  };
+
+  return (
+    <div id='register'>
+      <h2>PetGram</h2>
+      <p className='subtitle'>
+        Register now and view cute pets all over the world!
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Name'
+          onChange={(e) => setName(e.target.value)}
+          value={name || ''}
+        />
+        <input
+          type='email'
+          placeholder='Email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ''}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ''}
+        />
+        <input
+          type='password'
+          placeholder='Confirm your password'
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword || ''}
+        />
+        <input type='submit' value='Submit' />
+      </form>
+      <p>
+        Already a PetGrammer? <Link to='/login'>Login now</Link>!
+      </p>
+    </div>
+  );
 };
 
 export default Register;
---
+++ frontend/src/store.js
@@ -0,0 +1,6 @@
+// My "reducer with all reducers"
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {},
+});
